5735efdc9341c48c3d88c80f4e5cf64f6d3e66bf
Margot <76448401+togramort@users.noreply.github.com>
2022-03-16 00:30:38 +0000

    add sort function

--- src/server.c
+++ src/server.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 int compareGrades (double grades1, double grades2) {
   if (grades1 > grades2) {
     return 0;
@@ -9,7 +11,19 @@ int compareGrades (double grades1, double grades2) {
   return -1;
 }
 
+static int compare (const void * a, const void * b)
+{
+  if (*(double*)a > *(double*)b) return 1;
+  else if (*(double*)a < *(double*)b) return -1;
+  else return 0;  
+}
+
 int sortGrades (double *grades1, double *grades2) {
-  //sort
+  int size1 = sizeof(grades1) / sizeof(double);
+  qsort(grades1, size1, sizeof(double), compare);
+
+  int size2 = sizeof(grades2) / sizeof(double);
+  qsort(grades2, size2, sizeof(double), compare);
+
   return compareGrades (grades1[2], grades2[2]);
 }
